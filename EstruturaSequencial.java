15c85ae4062ae34feb47425513cebd345621c2ea
Christopher Lemos <114772952+ChristopherLemoss@users.noreply.github.com>
2023-08-03 23:00:21 -0300

    Create EstruturaSequencial.java
    Exercícios de estrtutura sequencial Java

---
+++ EstruturaSequencial.java
@@ -0,0 +1,45 @@
+	System.out.println("Você digitou o valor: " + x);
+	
+	
+	
+	sc.close();
+	Locale.setDefault(Locale.US);
+
+
+///////////////////////////////////////////////////////////////
+
+Locale.setDefault(Locale.US);
+	Scanner sc = new Scanner(System.in);
+	
+	char x;
+	x = sc.next().charAt(0);
+	
+	System.out.println("Você digitou o valor: " + x);
+	
+	
+	
+	sc.close();
+	Locale.setDefault(Locale.US);
+///////////////////////////////////////////////////////////
+
+
+Locale.setDefault(Locale.US);
+	Scanner sc = new Scanner(System.in);
+	
+	String x;
+	int y;
+	double z;
+	
+	x = sc.next();
+	y = sc.nextInt();
+	z = sc.nextDouble();
+	
+	
+	System.out.println("Você digitou o valor: " + x);
+	System.out.println(x);
+	System.out.println(y);
+	System.out.println(z);
+	
+	
+	sc.close();
+	Locale.setDefault(Locale.US);
